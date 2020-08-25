44ecb200aae30eaca47c88501b5aa2eb18761f6d
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-08-24 17:35:28 -0700

    not a function => add new to creating new instance

--- src/init.js
+++ src/init.js
@@ -21,12 +21,13 @@ $(document).ready(function() {
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
-
-    var dancer = dancerMakerFunction(
+    debugger;
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+    window.dancers.push(dancer);
     $('body').append(dancer.$node);
   });
 });
---
+++ src/jennyDancer.js
@@ -0,0 +1,28 @@
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  // var blinkyDancer = Dancer(top, left, timeBetweenSteps);
+
+  Dancer.call(this, top, left, timeBetweenSteps);
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  // this.oldStep = BlinkyDancer.step;
+
+  // return blinkyDancer;
+};
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+
+BlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle(); // on and off
+};
+
+
+// var jennydancer = new BlinkyDancer()
+// jennydancer.step()
